732d9ec2c2e362a547a42045d43b66080fd64186
Roman <skoronoviegod2@gmail.com>
2025-02-27 22:36:01 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,6 @@
 
 int main(){
     Sum(2,4);
+    Min(4,2);
+    std::cout<<"It's a change. Wow"<<std::endl;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 void Sum(int a, int b){
-    std::cout<<a+b;
+    std::cout<<a+b<<std::endl;
 }
 
 void Min(int a, int b){
-    std::cout<<a-b;
+    std::cout<<a-b<<std::endl;
 }
